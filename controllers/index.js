39a1ab11848e26c51ff668e36e69eb84807b03e6
Angel-L-T <lug21003@byui.edu>
2023-04-28 15:01:14 -0600

    L03 Changes Based on Solution

--- controllers/index.js
+++ controllers/index.js
@@ -28,23 +28,36 @@ const oneContactRoute = async (req, res, next) => {
 
 // This function creates a new contact
 const createContact = async (req, res, next) => {
-  mongodb
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+
+  const response = await mongodb
     .getDb()
     .db()
     .collection("contacts")
-    .insertOne(req.body)
-    .then((newContact) => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(201).json(newContact.insertedId); // Returning ID of new contact.
-    })
-    .catch((error) => console.log(error));
+    .insertOne(contact);
+
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res
+      .status(500)
+      .json(
+        response.error || "Some error occurred while creating the contact."
+      );
+  }
 };
 
 // This function updates a contact
 const updateContact = async (req, res, next) => {
   const userId = new ObjectId(req.params.id);
 
-  mongodb
+  const response = await mongodb
     .getDb()
     .db()
     .collection("contacts")
@@ -60,9 +73,8 @@ const updateContact = async (req, res, next) => {
         },
       }
     )
-    .then((updateResponse) => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(204).json(updateResponse); // Returning status
+    .then(() => {
+      res.status(204); // Returning status
     })
     .catch((error) => console.log(error));
 };
@@ -71,16 +83,21 @@ const updateContact = async (req, res, next) => {
 const deleteContact = async (req, res, next) => {
   const userId = new ObjectId(req.params.id);
 
-  mongodb
+  const response = await mongodb
     .getDb()
     .db()
     .collection("contacts")
-    .deleteOne({ _id: userId })
-    .then((deleteResponse) => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(200).json(deleteResponse.deletedCount); // Returning status
-    })
-    .catch((error) => console.log(error));
+    .deleteOne({ _id: userId });
+
+  if (response.deletedCount > 0) {
+    res.status(200).send();
+  } else {
+    res
+      .status(500)
+      .json(
+        response.error || "Some error occurred while deleting the contact."
+      );
+  }
 };
 
 module.exports = {
