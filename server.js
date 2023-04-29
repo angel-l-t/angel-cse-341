80f73054177a174de3679ff938ca010db2359355
Angel-L-T <lug21003@byui.edu>
2023-04-28 22:03:14 -0600

    Trying cors preflight

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
+const cors = require("cors");
 // Modules in my project
 const mongodb = require("./db/connect");
 const routes = require("./routes");
@@ -11,17 +12,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app
+  .options("*", cors())
   .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Acces-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
-    );
-    res.setHeader("Content-Type", "application/json");
-    res.setHeader("Acces-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    next();
-  }) // Right now it allows acces to all domains
   .use("/", routes);
 
 mongodb.initDb((err, mongodb) => {
